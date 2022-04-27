699a9a8b6b590d00497a8f4ec69449b7a2fd1952
jbusecke <julius@ldeo.columbia.edu>
2022-04-27 17:27:07 -0400

    fixing submodule

--- source/aerobulk/flux.py
+++ source/aerobulk/flux.py
@@ -1,9 +1,21 @@
+import numpy as np
 import aerobulk.aerobulk.mod_aerobulk_wrap as aero
 
 
 def flux_noskin(
-    sst, t_zt, hum_zt, u_zu, v_zu, slp, calgo="coare3p0", zt=10, zu=2, niter=1
+    sst, t_zt, hum_zt, u_zu, v_zu, slp=101000.0, algo="coare3p0", zt=10, zu=2, niter=1
 ):
+    # TODO: SLP is optional? seems like that is an option on the original code (https://github.com/brodeau/aerobulk/blob/7cd85ba8d05fd23eb81a8fdd202f11d9b63fa0b7/src/tests/aerobulk_toy.f90#L51)
+    # make sure that all inputs are proper 2d (or do we need 3d?) arrays
+    # TODO: Is there a way to handle this more clever? Maybe tom has an idea
+    # TODO: At least factoring this out might be a good idea, especially once we want to implement flux_skin
+
+    sst = np.atleast_2d(sst)
+    t_zt = np.atleast_2d(t_zt)
+    hum_zt = np.atleast_2d(hum_zt)
+    u_zu = np.atleast_2d(u_zu)
+    v_zu = np.atleast_2d(v_zu)
+
     return aero.mod_aerobulk_wrapper.aerobulk_model_noskin(
-        calgo, zt, zu, sst, t_zt, hum_zt, u_zu, v_zu, slp, niter
+        algo, zt, zu, sst, t_zt, hum_zt, u_zu, v_zu, slp, niter
     )
--- test/test.py
+++ test/test.py
@@ -24,3 +24,40 @@ def test_smoke_test():
     ql, qh, tau_x, tau_y, evap = flux_noskin(sst, t_zt, hum_zt, u_zu, v_zu, slp)
     toc = time()
     print(f"ORDER: {order} | TIME: {toc-tic}")
+
+
+def test_aerobulk_toy_results():
+    # This test aims to check the python API against the results posted in the README of
+    # aerobulk (https://github.com/brodeau/aerobulk#-giving-aerobulk-a-first-try-in-interactive-toy-mode)
+    niter = 8
+    zu = 10
+    zt = 2
+    sst = 22 + 273  # 22 C
+    t_zt = 20 + 273  # 20 C
+    hum_zt = 12  # g/kg
+    u_zu = 5
+    v_zu = 0
+
+    # start with only one algo
+    algo = "coare3p0"
+    expected_tau_x = 36.198
+    expected_evap = 3.1059
+    expected_ql = -88.031
+    expected_qh = -17.833
+
+    ql, qh, tau_x, tau_y, evap = flux_noskin(
+        sst,
+        t_zt,
+        hum_zt,
+        u_zu,
+        v_zu,
+        zu=zu,
+        zt=zt,
+        niter=niter,
+    )
+    # convert to the units displayed...geez this is convoluted...
+    evap = 3600 * 24 * evap
+    tau_x = 1e3 * tau_x
+
+    print(ql, qh, tau_x, evap)
+    assert 1 == 0
