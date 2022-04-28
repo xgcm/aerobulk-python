99367674c335ededfc80da3b92cad2d701f96c97
jbusecke <julius@ldeo.columbia.edu>
2022-04-28 19:05:07 -0400

    fix version

--- source/aerobulk/__init__.py
+++ source/aerobulk/__init__.py
@@ -1,1 +1,9 @@
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("aerobulk-python")
+except PackageNotFoundError:
+    __version__ = "unknown"
+    pass
+
 from .flux import flux_noskin
