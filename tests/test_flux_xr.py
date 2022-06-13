d47a299c8c58d680f04dccd8d06c6d4126aeceb3
Julius Busecke <julius@ldeo.columbia.edu>
2022-06-13 13:31:24 -0400

    Update tests/test_flux_xr.py
    Co-authored-by: Ryan Abernathey <ryan.abernathey@gmail.com>

--- tests/test_flux_xr.py
+++ tests/test_flux_xr.py
@@ -8,7 +8,7 @@ from aerobulk import noskin, skin
 """Tests for the xarray wrapper"""
 
 
-def create_data(shape: tuple, chunks: Dict[str, int] = {}, skin_correction=False):
+def create_data(shape: tuple[int, ...], chunks: Dict[str, int] = {}, skin_correction=False):
     def _arr(value, chunks):
         order = "F"  # Does this matter for the results?
         arr = xr.DataArray(np.full(shape, value, order=order))
