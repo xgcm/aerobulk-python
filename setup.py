7efc511135f6b613bff3534fbb9ad4fddda6382e
Julius Busecke <julius@ldeo.columbia.edu>
2024-02-22 11:20:00 -0600

    Support recent py versions

--- setup.py
+++ setup.py
@@ -101,6 +101,8 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
     ],
     install_requires=install_requires,
     python_requires=">=3.8",
