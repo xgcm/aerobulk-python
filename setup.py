aea056ef87969393a422f9025a6dcc905b618471
Julius Busecke <14314623+jbusecke@users.noreply.github.com>
2025-08-23 20:19:50 -0400

    Refactor setup.py for cleaner configuration

--- setup.py
+++ setup.py
@@ -1,33 +1,14 @@
 import os
-
-# We need to import setuptools here in order for it to persist in sys.modules.
-# Its presence/absence is used in subclassing setup in numpy/distutils/core.py.
-# However, we need to run the distutils version of sdist, so import that first
-# so that it is in sys.modules
 import numpy.distutils.command.sdist  # noqa
 import setuptools  # noqa
 from numpy.distutils.core import Extension, setup
 from numpy.distutils.fcompiler import get_default_fcompiler
-
-# Trying this from the numpy setup.py
-
-
-here = os.path.dirname(__file__)
-with open(os.path.join(here, "README.md"), encoding="utf-8") as f:
-    long_description = f.read()
-
-install_requires = [
-    "numpy",
-]
 
 # figure out which compiler we're going to use
 compiler = get_default_fcompiler()
 # set some fortran compiler-dependent flags
 f90flags = []
 if compiler == "gnu95":
-    # f90flags.append('-fno-range-check')
-    # f90flags.append('-ffree-form')
-    # These are the flags I used in the manual version (might have some more here)
     f90flags.append("-fdefault-real-8")
     f90flags.append("-ffree-line-length-200")
 elif compiler == "intel" or compiler == "intelem":
@@ -37,7 +18,7 @@ f90flags.append("-O3")
 #  Suppress all compiler warnings (avoid huge CI log files)
 f90flags.append("-w")
 
-# for this API we will only expose a single extension?
+# Extension modules
 ext_modules = [
     Extension(
         name="mod_aerobulk_wrap_noskin",
@@ -59,7 +40,6 @@ ext_modules = [
             "./source/fortran/mod_aerobulk_wrap_noskin.pyf",
         ],
         extra_f90_compile_args=f90flags,
-        # f2py_options=['--quiet'],
     ),
     Extension(
         name="mod_aerobulk_wrap_skin",
@@ -85,36 +65,8 @@ ext_modules = [
     ),
 ]
 
+# Minimal setup call - most configuration is now in pyproject.toml
 setup(
-    name="aerobulk-python",
-    description="General Circulation Model Postprocessing with xarray",
-    url="https://github.com/xgcm/aerobulk-python",
-    author="aerobulk-python Developers",
-    author_email="julius@ldeo.columbia.edu",
-    license="GPLv3",
-    classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
-        "Intended Audience :: Science/Research",
-        "Topic :: Scientific/Engineering",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-    ],
-    install_requires=install_requires,
-    python_requires=">=3.8, <3.12",
-    # long_description=long_description,
-    # long_description_content_type="text/x-rst",
-    setup_requires="setuptools_scm",
-    use_scm_version={
-        "write_to": "source/aerobulk/_version.py",
-        "write_to_template": '__version__ = "{version}"',
-        "tag_regex": r"^(?P<prefix>v)?(?P<version>[^\+]+)(?P<suffix>.*)?$",
-    },
-    package_dir={"": "source"},
-    packages=["aerobulk"],
     ext_package="aerobulk.aerobulk",
     ext_modules=ext_modules,
 )
